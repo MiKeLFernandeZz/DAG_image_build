4dae35fc88ef6968cc8a764593c34b37bf16c317
mfernandezlabastida <mfernandezlabastida@ikerlan.es>
2024-06-25 11:54:49 +0200

    mlflow task

--- dag.py
+++ dag.py
@@ -73,11 +73,80 @@ def DAG_image_build_dag():
             context=path,
             destination='registry-docker-registry.registry.svc.cluster.local:5001/mfernandezlabastida/engine:1.0',
             snapshot_mode=KanikoSnapshotMode.full,
+            # registry_username='username',
+            # registry_password='password',
+        )
+
+    @task.kubernetes(
+        image='mfernandezlabastida/kaniko:1.0',
+        name='image_build_2',
+        task_id='image_build_2',
+        namespace='airflow',
+        init_containers=[init_container],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        do_xcom_push=True,
+        container_resources=k8s.V1ResourceRequirements(
+            requests={'cpu': '0.5'},
+            limits={'cpu': '0.8'}
+        ),
+        env_vars=env_vars
+    )
+    def image_build_mlflow_task(run_id):
+        import mlflow
+        import os
+        import shutil
+        import logging
+        from kaniko import Kaniko, KanikoSnapshotMode
+
+        sys.path.insert(1, '/git/DAG_image_build')
+
+        try:
+            # Obtener información del run
+            run_info = mlflow.get_run(run_id)
+            artifact_uri = run_info.info.artifact_uri + '/model/requirements.txt'
+
+            print(f"URI de los artefactos del run: {artifact_uri}")
+
+            # Descargar el artefacto requirements.txt a una carpeta temporal
+            tmp_artifact_dir = "./artifacts"
+            mlflow.artifacts.download_artifacts(run_id=run_id, dst_path=tmp_artifact_dir, artifact_path='model/requirements.txt')
+
+            # Verificar si el archivo fue descargado correctamente
+            downloaded_file = os.path.join(tmp_artifact_dir, 'model', 'requirements.txt')
+            print(downloaded_file)
+            if os.path.exists(downloaded_file):
+                # Mover el archivo descargado a la ubicación deseada
+                shutil.move(downloaded_file, './docker/requirements.txt')
+                print("Archivo requirements.txt movido exitosamente.")
+            else:
+                print(f"Error: No se pudo descargar el archivo requirements.txt desde {artifact_uri}.")
+        except mlflow.exceptions.MlflowException as e:
+            print(f"Error de MLflow: {e}")
+        except FileNotFoundError:
+            print(f"Error: No se encontró el archivo en la ruta especificada.")
+        except PermissionError:
+            print(f"Error: Permiso denegado para acceder al archivo o directorio.")
+        except Exception as e:
+            print(f"Error inesperado: {str(e)}")
+
+        path = './docker'
+
+        logging.warning("Building and pushing image")
+        kaniko = Kaniko()
+        kaniko.build(
+            dockerfile=f'{path}/Dockerfile',
+            context=path,
+            destination='registry-docker-registry.registry.svc.cluster.local:5001/mfernandezlabastida/engine:1.1',
+            snapshot_mode=KanikoSnapshotMode.full,
+            # registry_username='username',
+            # registry_password='password',
         )
 
     image_build_result = image_build_task()
+    image_build_mlflow_result = image_build_mlflow_task('51e9022a0c2442da9c6e7b130b56defc')
     
     # Define the order of the pipeline
-    image_build_result
+    [image_build_result, image_build_mlflow_result]
 # Call the DAG 
 DAG_image_build_dag()
